e296fee084a48b7164503b0da1ff2d1862f25b60
Zulfiqor Rustamov <zulfiqorrustamov81@gmail.com>
2025-10-20 23:10:05 +0500

    Add role claim

---
+++ Udemy.Infrastructure/Authorization/UdemyUserClaimsPrincipalFactory.cs.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using Udemy.Domain.Entities;
+
+namespace Udemy.Infrastructure.Authorization;
+
+public class UdemyUserClaimsPrincipalFactory(
+    UserManager<User> userManager,
+    RoleManager<IdentityRole> roleManager,
+    IOptions<IdentityOptions> options) : UserClaimsPrincipalFactory<User, IdentityRole>(userManager, roleManager, options) 
+{
+    public override Task<ClaimsPrincipal> CreateAsync(User user)
+    {
+        var id = GenerateClaimsAsync(user);
+
+        
+    }
+}
